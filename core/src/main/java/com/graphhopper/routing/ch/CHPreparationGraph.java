68dae25670d808b44a3630f7b5972eddf92c0bd9
easbar <easbar.mail@posteo.net>
2020-09-14 13:21:50 +0200

    combine inout

--- core/src/main/java/com/graphhopper/routing/ch/CHPreparationGraph.java
+++ core/src/main/java/com/graphhopper/routing/ch/CHPreparationGraph.java
@@ -42,8 +42,7 @@ public class CHPreparationGraph {
     private final int edges;
     private final boolean edgeBased;
     private final TurnCostFunction turnCostFunction;
-    private MyListOfLists outEdges;
-    private MyListOfLists inEdges;
+    private MyListOfLists inoutedges;
     private IntSet neighborSet;
     private OrigGraph origGraph;
     private OrigGraph.Builder origGraphBuilder;
@@ -68,12 +67,10 @@ public class CHPreparationGraph {
         this.nodes = nodes;
         this.edges = edges;
         this.edgeBased = edgeBased;
-        outEdges = new MyListOfLists(nodes, 2);
-        inEdges = new MyListOfLists(nodes, 2);
+        inoutedges = new MyListOfLists(nodes, 2);
         origGraphBuilder = edgeBased ? new OrigGraph.Builder() : null;
         neighborSet = new IntHashSet();
         nextShortcutId = edges;
-
     }
 
     public static void buildFromGraph(CHPreparationGraph prepareGraph, Graph graph, Weighting weighting) {
@@ -88,7 +85,7 @@ public class CHPreparationGraph {
         while (iter.next()) {
             double weightFwd = iter.get(accessEnc) ? weighting.calcEdgeWeight(iter, false) : Double.POSITIVE_INFINITY;
             double weightBwd = iter.getReverse(accessEnc) ? weighting.calcEdgeWeight(iter, true) : Double.POSITIVE_INFINITY;
-            prepareGraph.addEdge(iter.getBaseNode(), iter.getAdjNode(), iter.getEdge(), (float) weightFwd, (float) weightBwd);
+            prepareGraph.addEdge(iter.getBaseNode(), iter.getAdjNode(), iter.getEdge(), weightFwd, weightBwd);
         }
         prepareGraph.prepareForContraction();
     }
@@ -162,20 +159,20 @@ public class CHPreparationGraph {
     }
 
     public int getDegree(int node) {
-        return outEdges.getEdges(node) + inEdges.getEdges(node);
+        return inoutedges.getEdges(node);
     }
 
-    public void addEdge(int from, int to, int edge, float weightFwd, float weightBwd) {
+    public void addEdge(int from, int to, int edge, double weightFwd, double weightBwd) {
         checkNotReady();
         boolean fwd = Double.isFinite(weightFwd);
         boolean bwd = Double.isFinite(weightBwd);
         if (!fwd && !bwd)
             return;
-        PrepareEdge prepareEdge = new PrepareBaseEdge(edge, from, to, weightFwd, weightBwd);
-        outEdges.add(from, prepareEdge);
-        inEdges.add(to, prepareEdge);
-        outEdges.add(to, prepareEdge);
-        inEdges.add(from, prepareEdge);
+        // todonow: is it ok to cast to float? maybe add some check that asserts certain precision? especially inf?
+        PrepareBaseEdge prepareEdge = new PrepareBaseEdge(edge, from, to, (float) weightFwd, (float) weightBwd);
+        inoutedges.add(from, prepareEdge);
+        if (from != to)
+            inoutedges.add(to, prepareEdge);
 
         if (edgeBased)
             origGraphBuilder.addEdge(from, to, edge, fwd, bwd);
@@ -187,8 +184,9 @@ public class CHPreparationGraph {
         PrepareEdge prepareEdge = edgeBased
                 ? new EdgeBasedPrepareShortcut(nextShortcutId, from, to, origEdgeKeyFirst, origEdgeKeyLast, weight, skipped1, skipped2, origEdgeCount)
                 : new PrepareShortcut(nextShortcutId, from, to, weight, skipped1, skipped2, origEdgeCount);
-        outEdges.add(from, prepareEdge);
-        inEdges.add(to, prepareEdge);
+        inoutedges.add(from, prepareEdge);
+        if (from != to)
+            inoutedges.add(to, prepareEdge);
         return nextShortcutId++;
     }
 
@@ -202,12 +200,12 @@ public class CHPreparationGraph {
 
     public PrepareGraphEdgeExplorer createOutEdgeExplorer() {
         checkReady();
-        return new PrepareGraphEdgeExplorerImpl(outEdges, false);
+        return new PrepareGraphEdgeExplorerImpl(inoutedges, false);
     }
 
     public PrepareGraphEdgeExplorer createInEdgeExplorer() {
         checkReady();
-        return new PrepareGraphEdgeExplorerImpl(inEdges, true);
+        return new PrepareGraphEdgeExplorerImpl(inoutedges, true);
     }
 
     public PrepareGraphOrigEdgeExplorer createOutOrigEdgeExplorer() {
@@ -234,37 +232,24 @@ public class CHPreparationGraph {
         // node ids
         neighborSet.clear();
         IntArrayList neighbors = new IntArrayList(getDegree(node));
-        for (int i = 0; i < outEdges.getEdges(node); i++) {
-            PrepareEdge prepareEdge = outEdges.get(node, i);
+        for (int i = 0; i < inoutedges.getEdges(node); i++) {
+            PrepareEdge prepareEdge = inoutedges.get(node, i);
             int adjNode = prepareEdge.getNodeB();
             if (adjNode == node)
                 adjNode = prepareEdge.getNodeA();
             if (adjNode == node)
                 continue;
-            inEdges.remove(adjNode, prepareEdge);
+            inoutedges.remove(adjNode, prepareEdge);
             if (neighborSet.add(adjNode))
                 neighbors.add(adjNode);
         }
-        for (int i = 0; i < inEdges.getEdges(node); i++) {
-            PrepareEdge prepareEdge = inEdges.get(node, i);
-            int adjNode = prepareEdge.getNodeA();
-            if (adjNode == node)
-                adjNode = prepareEdge.getNodeB();
-            if (adjNode == node)
-                continue;
-            outEdges.remove(adjNode, prepareEdge);
-            if (neighborSet.add(adjNode))
-                neighbors.add(adjNode);
-        }
-        outEdges.clear(node);
-        inEdges.clear(node);
+        inoutedges.clear(node);
         return neighbors;
     }
 
     public void close() {
         checkReady();
-        outEdges = null;
-        inEdges = null;
+        inoutedges = null;
         neighborSet = null;
         if (edgeBased)
             origGraph = null;
@@ -306,10 +291,17 @@ public class CHPreparationGraph {
 
         @Override
         public boolean next() {
-            index++;
-            boolean result = index < prepareEdges.getEdges(node);
-            currEdge = result ? prepareEdges.get(node, index) : null;
-            return result;
+            while (true) {
+                index++;
+                boolean result = index < prepareEdges.getEdges(node);
+                if (!result) {
+                    currEdge = null;
+                    return false;
+                }
+                currEdge = prepareEdges.get(node, index);
+                if (!currEdge.isShortcut() || ((!reverse && currEdge.getNodeA() == node) || (reverse && currEdge.getNodeB() == node)))
+                    return true;
+            }
         }
 
         @Override
